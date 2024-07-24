511a258261c271ef4ea0854696a0f3c9b814580f
Dima <dima2000yan@gmail.com>
2024-07-24 11:47:00 +0300

    created list views and templates for models

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo_list_app.models import Tag, Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
