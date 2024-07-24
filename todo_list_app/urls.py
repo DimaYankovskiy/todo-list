84a287638bbb65e44be26ad5710778d4c3126ce0
Dima <dima2000yan@gmail.com>
2024-07-24 13:21:44 +0300

    added create/update/delete views and templates for Task model

--- todo_list_app/urls.py
+++ todo_list_app/urls.py
@@ -5,11 +5,14 @@ from todo_list_app.views import (
     TagListView,
     TagCreateView,
     TagUpdateView,
-    TagDeleteView
+    TagDeleteView, TaskCreateView, TaskUpdateView, TaskDeleteView
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -8,6 +8,29 @@ from todo_list_app.models import Tag, Task
 class TaskListView(generic.ListView):
     model = Task
 
+    def get_queryset(self):
+        return Task.objects.order_by('is_done', '-created_at')
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = ["content", "deadline", "tags"]
+    success_url = reverse_lazy("todo_list_app:task-list")
+    template_name = "todo_list_app/task_form.html"
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = ["content", "deadline", "tags"]
+    success_url = reverse_lazy("todo_list_app:task-list")
+    template_name = "todo_list_app/task_form.html"
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo_list_app:task-list")
+    template_name = "todo_list_app/task_confirm_delete.html"
+
 
 class TagListView(generic.ListView):
     model = Tag
